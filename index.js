a8fc90a8e6271ea66262bc44e62724800265f0d8
Umesh Bilagi <umeshbilagi@gmail.com>
2021-01-07 21:11:50 +0530

    done

--- index.js
+++ index.js
@@ -42,8 +42,9 @@ csv()
             "Item Total" : parseFloat(r["Item Total"]),
 
 
-            "CGST 2.5" : parseFloat(r["CGST Rate %"]) === 2.5 ? parseFloat(r["CGST(FCY)"]) : 0,
-            "SGST 2.5" : parseFloat(r["SGST Rate %"]) === 2.5 ? parseFloat(r["SGST(FCY)"]) : 0,
+            "CGST 6" : parseFloat(r["CGST Rate %"]) === 6 ? parseFloat(r["CGST(FCY)"]) : 0,
+            "SGST 6" : parseFloat(r["SGST Rate %"]) === 6 ? parseFloat(r["SGST(FCY)"]) : 0,
+
 
             "CGST 6" : parseFloat(r["CGST Rate %"]) === 6 ? parseFloat(r["CGST(FCY)"]) : 0,
             "SGST 6" : parseFloat(r["SGST Rate %"]) === 6 ? parseFloat(r["SGST(FCY)"]) : 0,
